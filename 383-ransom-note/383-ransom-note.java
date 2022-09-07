0bce77d4f25fec4e6ca4ba5443fb477bf2bba09b
Unnati Babruwad <40277323+babruwadunnati@users.noreply.github.com>
2022-09-07 20:37:22 +0530

    Time: 5 ms (68.90%), Space: 46.5 MB (50.97%) - LeetHub

--- 383-ransom-note/383-ransom-note.java
+++ 383-ransom-note/383-ransom-note.java
@@ -2,35 +2,35 @@ class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
         
         //Somehow taking longer time
-        if(ransomNote.length()>magazine.length()) return false;
-        HashMap<Character,Integer> hm1=new HashMap<>();
-        for(char c:magazine.toCharArray()){
-            if(hm1.containsKey(c)){
-                int val=hm1.get(c);
-                hm1.put(c,val+1);
-            }else
-                hm1.put(c,1);
+//         if(ransomNote.length()>magazine.length()) return false;
+//         HashMap<Character,Integer> hm1=new HashMap<>();
+//         for(char c:magazine.toCharArray()){
+//             if(hm1.containsKey(c)){
+//                 int val=hm1.get(c);
+//                 hm1.put(c,val+1);
+//             }else
+//                 hm1.put(c,1);
                 
+//         }
+//         for(char c:ransomNote.toCharArray()){
+//             if(!hm1.containsKey(c)||hm1.get(c)<=0){
+//                 return false;
+//             }else
+//                 hm1.put(c, hm1.get(c)-1);
+//         }
+//         return true;
+        
+        if(ransomNote.length()>magazine.length()) return false;
+        int[] arr = new int[26];
+        for(char c:magazine.toCharArray()){
+            arr[c-'a']++;
         }
         for(char c:ransomNote.toCharArray()){
-            if(!hm1.containsKey(c)||hm1.get(c)<=0){
+            if(arr[c-'a']<=0)
                 return false;
-            }else
-                hm1.put(c, hm1.get(c)-1);
+            else
+                arr[c-'a']--;
         }
         return true;
-        
-    //     if(ransomNote.length()>magazine.length()) return false;
-    //     int[] arr = new int[26];
-    //     for(char c:magazine.toCharArray()){
-    //         arr[c-'a']++;
-    //     }
-    //     for(char c:ransomNote.toCharArray()){
-    //         if(arr[c-'a']<=0)
-    //             return false;
-    //         else
-    //             arr[c-'a']--;
-    //     }
-    //     return true;
      }
 }
