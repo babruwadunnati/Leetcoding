b4a65cec17ff2f4075d77ccf3e4e220e95bafc21
Unnati Babruwad <40277323+babruwadunnati@users.noreply.github.com>
2022-09-07 20:23:51 +0530

    Time: 43 ms (12.10%), Space: 54 MB (7.55%) - LeetHub

--- 383-ransom-note/383-ransom-note.java
+++ 383-ransom-note/383-ransom-note.java
@@ -10,13 +10,10 @@ class Solution {
                 
         }
         for(char c:ransomNote.toCharArray()){
-            if(hm1.containsKey(c)){
-                int val=hm1.get(c);
-                hm1.put(c,val-1);
-                if(val==1)
-                    hm1.remove(c);
-            }else
+            if(!hm1.containsKey(c)||hm1.get(c)<=0){
                 return false;
+            }else
+                hm1.put(c, hm1.get(c)-1);
         }
         return true;
     }
