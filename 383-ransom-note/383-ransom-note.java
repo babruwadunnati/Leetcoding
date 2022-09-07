321ced4d7865fd31d798603851cdc6e6e0f00928
Unnati Babruwad <40277323+babruwadunnati@users.noreply.github.com>
2022-09-07 20:35:58 +0530

    Time: 5 ms (68.90%), Space: 46.6 MB (47.82%) - LeetHub

--- 383-ransom-note/383-ransom-note.java
+++ 383-ransom-note/383-ransom-note.java
@@ -1,19 +1,34 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        HashMap<Character,Integer> hm1=new HashMap<>();
-        for(char c:magazine.toCharArray()){
-            if(hm1.containsKey(c)){
-                int val=hm1.get(c);
-                hm1.put(c,val+1);
-            }else
-                hm1.put(c,1);
+        
+        //Somehow taking longer time
+//         HashMap<Character,Integer> hm1=new HashMap<>();
+//         for(char c:magazine.toCharArray()){
+//             if(hm1.containsKey(c)){
+//                 int val=hm1.get(c);
+//                 hm1.put(c,val+1);
+//             }else
+//                 hm1.put(c,1);
                 
+//         }
+//         for(char c:ransomNote.toCharArray()){
+//             if(!hm1.containsKey(c)||hm1.get(c)<=0){
+//                 return false;
+//             }else
+//                 hm1.put(c, hm1.get(c)-1);
+//         }
+//         return true;
+        
+        if(ransomNote.length()>magazine.length()) return false;
+        int[] arr = new int[26];
+        for(char c:magazine.toCharArray()){
+            arr[c-'a']++;
         }
         for(char c:ransomNote.toCharArray()){
-            if(!hm1.containsKey(c)||hm1.get(c)<=0){
+            if(arr[c-'a']<=0)
                 return false;
-            }else
-                hm1.put(c, hm1.get(c)-1);
+            else
+                arr[c-'a']--;
         }
         return true;
     }
