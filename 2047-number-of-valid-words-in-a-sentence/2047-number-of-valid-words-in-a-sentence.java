a2b07f2dd0513bd5630bc8b8e4f194cc8fd35f9a
Unnati Babruwad <40277323+babruwadunnati@users.noreply.github.com>
2022-09-08 16:17:51 +0530

    Time: 20 ms (39.47%), Space: 44.3 MB (60.53%) - LeetHub

--- 2047-number-of-valid-words-in-a-sentence/2047-number-of-valid-words-in-a-sentence.java
+++ 2047-number-of-valid-words-in-a-sentence/2047-number-of-valid-words-in-a-sentence.java
@@ -1,10 +1,8 @@
 class Solution {
     public int countValidWords(String sentence) {
-       // String sene = "  Hi  and     hello ";
         String[] splited = sentence.strip().split("\\s+");
         int count =0 ;
         for(int i=0;i<splited.length;i++){
-           // System.out.println(";"+splited[i]+";");
             count+=checkValid(splited[i]);
         }
         return count;
@@ -19,9 +17,7 @@ class Solution {
             else if(c=='-'){
                 if(hyphen==1||i==0||i==word.length()-1)return 0;
                 else if(!(Character.isLowerCase(word.charAt(i-1)) &&Character.isLowerCase(word.charAt(i+1))))
-                    return 0;
-                //else if(i==word.length()-1||!)
-                         
+                    return 0;      
                 hyphen=1;
             }else if(c==','||c=='.'||c=='!'){
                 if(i!=word.length()-1|| punctuation==1)
