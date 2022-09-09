43c04944676cbc868b5e6c10f57d3ce5313a5506
Unnati Babruwad <40277323+babruwadunnati@users.noreply.github.com>
2022-09-09 19:25:47 +0530

    Time: 6 ms (5.80%), Space: 41.5 MB (81.35%) - LeetHub

--- 290-word-pattern/290-word-pattern.java
+++ 290-word-pattern/290-word-pattern.java
@@ -2,12 +2,12 @@ class Solution {
     public boolean wordPattern(String pattern, String s) {
         HashMap<Character,String> map= new HashMap<>();
         String[] splited = s.split("\\s+");
-        if(splited.length!=pattern.length()) return false;
+        if(splited.length!=pattern.length()) 
+            return false;
         for(int i=0;i<pattern.length();i++){
             char c=pattern.charAt(i);
             if(map.containsKey(c)){
-                String temp = map.get(c);
-                if(!(temp.toString().equals(splited[i].toString())))
+                if(!(map.get(c).toString().equals(splited[i].toString())))
                     return false;   
             }else{
                 if(map.containsValue(splited[i]))
