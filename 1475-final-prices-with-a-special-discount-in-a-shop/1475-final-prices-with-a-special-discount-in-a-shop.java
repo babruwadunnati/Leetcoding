089dcb9ac0e90e2b0ab77c311c9347f875b4e3df
Unnati Babruwad <40277323+babruwadunnati@users.noreply.github.com>
2022-09-22 15:58:52 +0530

    Time: 3 ms (64.05%), Space: 42.2 MB (95.90%) - LeetHub

---
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] finalPrices(int[] prices) {
+        int[] ans = new int[prices.length];
+      //  ans=prices;
+        int j=0;
+        for(int i=0;i<prices.length-1;i++){
+            j=i+1;
+            while(j!=prices.length&& prices[j]>prices[i])
+                j++;
+            if(j!=prices.length){
+                ans[i]=prices[i]-prices[j];
+                continue;
+            }
+            if(j==prices.length)
+                ans[i]=prices[i];
+        }
+        System.out.println(j);
+        
+        ans[prices.length-1]=prices[prices.length-1];
+        return ans;
+        
+    }
+}
