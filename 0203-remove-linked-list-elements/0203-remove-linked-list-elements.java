01f84c791757cd006e9a30dacfd300facfc7a6b4
Unnati Babruwad <40277323+babruwadunnati@users.noreply.github.com>
2022-11-04 12:50:59 +0530

    Time: 5 ms (23.72%), Space: 48.5 MB (79.67%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.java
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.java
@@ -10,23 +10,38 @@
  */
 class Solution {
     public ListNode removeElements(ListNode head, int val) {
-        ListNode prev = new ListNode();
-        ListNode curr = new ListNode();
-        if(head==null)
-            return head;
-        curr=head;
-        prev=curr;
+        
+        //Solution 1
+//         ListNode prev = new ListNode();
+//         ListNode curr = new ListNode();
+//         if(head==null)
+//             return head;
+//         curr=head;
+//         prev=curr;
       
-        while(curr!=null){
-            if(curr.val==val)
-                prev.next=curr.next;
-            else
-                prev=curr;
-            curr=curr.next;
+//         while(curr!=null){
+//             if(curr.val==val)
+//                 prev.next=curr.next;
+//             else
+//                 prev=curr;
+//             curr=curr.next;
             
-        }
-          if(head.val==val){
-                head=head.next;
+//         }
+//           if(head.val==val){
+//                 head=head.next;
+//         }
+//         return head;
+        
+        //Solution 2
+        while(head!=null && head.val==val)
+            head=head.next;
+        ListNode curr = new ListNode();
+        curr = head;
+        while(curr!=null&&curr.next!=null){
+            if(curr.next.val==val)
+                curr.next = curr.next.next;
+            else
+                curr=curr.next;
         }
         return head;
     }
